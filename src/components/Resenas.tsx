1ea8cfaaeeb121a724cc414efe53e701b9b85087
Emaa27 <gefrias.27@gmail.com>
2026-03-25 11:35:31 -0300

    adaptado el resenas al global css

--- src/components/Resenas.tsx
+++ src/components/Resenas.tsx
@@ -243,7 +243,7 @@ export default function Resenas() {
   const scrollNext = useCallback(() => emblaApi?.scrollNext(), [emblaApi]);
 
   return (
-    <section id="resenas" className="py-20 px-4 md:px-6 bg-[#ededed]">
+    <section id="resenas" className="py-20 px-4 md:px-6 bg-resenas-bg">
       <div className="mx-auto max-w-[1400px]">
         {/* Header */}
         <motion.div
