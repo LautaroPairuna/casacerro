cd7853e56fa27c7025196c7c74da75bb6cab0e77
Emaa27 <gefrias.27@gmail.com>
2026-03-25 12:41:31 -0300

    adaptando colores a resenas y a eso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
       <Header />
       <Hero />
       <Nosotros />
+      <Resenas />
+      <Habitaciones />
       <Tarifas />
       <Contacto />
       <WhatsappLink />
